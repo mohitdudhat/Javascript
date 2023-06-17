521cea65551ad63a378a5d47f4a920cf0480ec9d
mohitdudhat <355mohitdudhat@gmail.com>
2023-06-17 07:19:58 -0700

    PR 10 Forgot Password

--- PR 10/Sign up.js
+++ PR 10/Sign up.js
@@ -16,9 +16,16 @@ let save = (name, email, password, confirmPassword) => {
       data.push(obj);
       localStorage.setItem("User", JSON.stringify(data));
     } else {
-      data = getData;
-      data.push(obj);
-      localStorage.setItem("User", JSON.stringify(data));
+      const isEmailExists = getData.some((item) => item.email === email);
+      if (isEmailExists) {
+        alert("Email already exists");
+        window.location.href = "Sign up.html";
+        return;
+      } else {
+        data = getData;
+        data.push(obj);
+        localStorage.setItem("User", JSON.stringify(data));
+      }
     }
     document.getElementById("name").value = "";
     document.getElementById("email").value = "";
@@ -27,42 +34,34 @@ let save = (name, email, password, confirmPassword) => {
   } else {
     alert("Password and Confirm Password Does not Match");
   }
+  window.location.href = "index.html";
 };
 function validateForm() {
-  // Get form input values
   var nameInput = document.getElementById("name").value;
   var emailInput = document.getElementById("email").value;
   var passwordInput = document.getElementById("password").value;
   var confirmPasswordInput = document.getElementById("confirmPassword").value;
 
-  // Validate name field (not empty)
   if (nameInput === "") {
     alert("Please enter your name.");
     return;
   }
 
-  // Validate email field (valid email address)
   var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailPattern.test(emailInput)) {
     alert("Please enter a valid email address.");
     return;
   }
 
-  // Validate password field (at least 8 characters)
   if (passwordInput.length < 8) {
     alert("Password must be at least 8 characters long.");
     return;
   }
 
-  // Validate confirm password field (matches password)
   if (passwordInput !== confirmPasswordInput) {
     alert("Passwords do not match.");
     return;
   }
-
-  // If all validations pass, submit the form
-  alert("Registration successful!");
   document.getElementById("remember-me").checked = false;
   save(nameInput, emailInput, passwordInput, confirmPasswordInput);
-  window.location.href = "index.html";
 }
--- PR 10/script.js
+++ PR 10/script.js
@@ -24,3 +24,36 @@ let check = () => {
     alert("Your Email is not Found");
   }
 };
+let forgotPassword = () => {
+  let title = document.querySelector(".title");
+  let content = document.querySelector(".content");
+  let passwordField = document.querySelector(".password");
+  let loginButton = document.querySelector("input[value='Login']");
+
+  title.innerText = "Forgot password";
+  content.style.display = "none";
+  passwordField.style.display = "none";
+  loginButton.value = "Send OTP";
+  loginButton.onclick = OTP;
+};
+let OTP = () => {
+  let otp = "";
+  for (let i = 0; i < 6; i++) {
+    otp += Math.floor(Math.random() * 10);
+  }
+  alert(`Your OTP is ${otp}`);
+  let otpObj = {
+    email: document.getElementById("email").value,
+    otp: otp,
+  };
+  localStorage.setItem("OTP", JSON.stringify(otpObj));
+  let loginButton = document.querySelector("input[value='Send OTP']");
+  let enteredOTP = prompt("Enter your OTP");
+  let storedOTP = JSON.parse(localStorage.getItem("OTP")).otp;
+
+  if (enteredOTP === storedOTP) {
+    window.location.href = "newPassword.html";
+  } else {
+    alert("Incorrect OTP. Please try again.");
+  }
+};
